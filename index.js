178b19ec75157fe84ccaa22800254aa8024517ac
nascherman <n_scherman@hotmail.com>
2016-05-31 18:21:19 -0400

    charCode organization

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes
 var assetsPath = './assets/'; 
 var fontFace;
 
-var fontLibrary = [
+var alphaLibrary = [
 	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-	'w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-	'Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','^','&','*','(',')','?'
+	'w','x','y','z'
 ];
-fontLibrary.forEach(function(item) {
-	fontLibrary.push(item.toUpperCase() );
-})
+
 
 const directorySearch = require('directory-search');
 MockBrowser = require('mock-browser').mocks.MockBrowser;
@@ -32,7 +29,7 @@ if(process.argv[3]) {
 }
 
 if(process.argv[4]) {
-	fontLibrary = process.argv[4].split('');
+	alphaLibrary = process.argv[4].split('');
 }
 var dirName = assetsPath + fontFace.split('.')[0];
 try {
@@ -42,14 +39,24 @@ try {
 }
 
 TextToSVG.load(assetsPath + fontFace, function(err, library) {
-	fontLibrary.forEach(function(char) {
+	alphaLibrary.forEach(function(char) {
 		const svg = library.getSVG(char, options);
-		fs.writeFile(dirName + '/' + char +'.svg', svg, function(err) {
+		fs.writeFile(dirName + '/' + char.charCodeAt(0) +'.svg', svg, function(err) {
 			if(err) console.log(err);
 			else {
 				console.log('Creating font lib for ' + char );
 			}
 		});
 	});
+	alphaLibrary.forEach(function(char) {
+		const svg = library.getSVG(char.toUpperCase(), options);
+		fs.writeFile(dirName + '/' + char.toUpperCase().charCodeAt(0) +'.svg', svg, function(err) {
+			if(err) console.log(err);
+			else {
+				console.log('Creating font lib for ' + char.toUpperCase() );
+			}
+		});
+	});
+
 });
   
