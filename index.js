879ba6cfae07c691839e2c5830d1c54ac6cf493f
nascherman <n_scherman@hotmail.com>
2016-06-03 16:18:35 -0400

    transform multiple ttf or otf files

--- index.js
+++ index.js
@@ -2,61 +2,85 @@
 const fs = require('fs');
 const TextToSVG = require('text-to-svg');
 
-const attributes = {fill: 'red', stroke: 'black'};
-const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+const attributes = {fill: 'black', stroke: 'black'};
+const options = {x: 0, y: 0, fontSize: 32, anchor: 'top', attributes: attributes};
 
-var assetsPath = './assets/'; 
+var assetsPath = './'; 
+var type = '.ttf'
 var fontFace;
 
 var alphaLibrary = [
 	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
 	'w','x','y','z'
 ];
-
+var charLibrary = [
+	'!','@','#','$','%','^','&','*','(',')',':',';',
+	'{','}','|','?','>','<','+','_','-','/','\'','~','`',
+	'1','2','3','4','5','6','7','8','9','0','[',']','\"',
+	',','.','=','\\'
+];
 
 const directorySearch = require('directory-search');
 MockBrowser = require('mock-browser').mocks.MockBrowser;
 
 if(!process.argv[2]) {
-	console.log('must provide a font');
-}
-else {
-	fontFace = process.argv[2];
+	console.log('must provide an asset path');
 }
 
+
 if(process.argv[3]) {
-	assetsPath = process.argv[3];
+	type = process.argv[3];
 }
 
 if(process.argv[4]) {
 	alphaLibrary = process.argv[4].split('');
 }
-var dirName = assetsPath + fontFace.split('.')[0];
-try {
-	fs.mkdirSync(dirName );
-} catch (err) {
-	console.warn(err);
+if(process.argv[5]) {
+	alphaLibrary = process.argv[4].split('');
 }
 
-TextToSVG.load(assetsPath + fontFace, function(err, library) {
-	alphaLibrary.forEach(function(char) {
-		const svg = library.getSVG(char, options);
-		fs.writeFile(dirName + '/' + char.charCodeAt(0) +'.svg', svg, function(err) {
-			if(err) console.log(err);
-			else {
-				console.log('Creating font lib for ' + char );
-			}
-		});
-	});
-	alphaLibrary.forEach(function(char) {
-		const svg = library.getSVG(char.toUpperCase(), options);
-		fs.writeFile(dirName + '/' + char.toUpperCase().charCodeAt(0) +'.svg', svg, function(err) {
-			if(err) console.log(err);
-			else {
-				console.log('Creating font lib for ' + char.toUpperCase() );
-			}
-		});
-	});
 
+
+directorySearch(process.argv[2], type, function(err, results) {
+	results.forEach( (result) => {
+		var dirName = assetsPath + result.split('.');
+		try {
+			fs.mkdirSync(dirName );
+		} catch (err) {
+			console.warn(err);
+		}
+		TextToSVG.load(assetsPath + result, function(err, library) {
+			if(err) console.warn(err);
+			else {
+				alphaLibrary.forEach(function(char) {
+					const svg = library.getSVG(char, options);
+					fs.writeFile(dirName + '/' + char.charCodeAt(0) +'.svg', svg, function(err) {
+						if(err) console.log(err);
+						else {
+							console.log('Creating font lib for ' + char );
+						}
+					});
+				});
+				alphaLibrary.forEach(function(char) {
+					const svg = library.getSVG(char.toUpperCase(), options);
+					fs.writeFile(dirName + '/' + char.toUpperCase().charCodeAt(0) +'.svg', svg, function(err) {
+						if(err) console.log(err);
+						else {
+							console.log('Creating font lib for ' + char.toUpperCase() );
+						}
+					});
+				});
+				charLibrary.forEach(function(char) {
+					const svg = library.getSVG(char, options);
+					fs.writeFile(dirName + '/' + char.charCodeAt(0) +'.svg', svg, function(err) {
+						if(err) console.log(err);
+						else {
+							console.log('Creating font lib for ' + char );
+						}
+					});
+				});
+			}
+		});	
+	});
 });
   
